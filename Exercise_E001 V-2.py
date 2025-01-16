ceebd38f783f79a1be4361c7d3ef9ef8197035f7
HetviJesadiya <hetvij.reliution@gmail.com>
2025-01-16 16:34:24 +0530

    Exercise_E001 V-2.py

--- Exercise_E001 V-2.py
+++ Exercise_E001 V-2.py
@@ -1,5 +1,5 @@
-'''
-Exercise: E001-V2
+
+'''Exercise: E001-V2
 •	Add new data members “parent”, “display_name”, and “products” (list of product objects) inside the category class.
 •	Add a new member function to generate “display_name”.
 •	“display_name” has the text value as below.
@@ -13,12 +13,13 @@ Exercise: E001-V2
 '''
 
 class Product:
-    def __init__(self, name, price):
+    def __init__(self, name, category, price):
         self.name = name
+        self.category = category  
         self.price = price
 
     def __str__(self):
-        return f"{self.name} {self.price}"
+        return f"{self.name} ({self.category.display_name}) {self.price}"
 
 
 class Category:
@@ -30,16 +31,30 @@ class Category:
         self.name = name
         self.parent = parent
         self.display_name = self.generate_display_name()
-        self.products = products if products else[]
+        self.products = products if products else []
 
     def generate_display_name(self):
         return f"{self.parent.display_name} > {self.name}" if self.parent else self.name
 
-
     def __str__(self):
         product_list = "\n  ".join(map(str, self.products)) if self.products else "No Products"
         return f"{self.code} | {self.display_name}\n  {product_list}"
+    
+    def sorting_categories(categories):
+        for i in range(len(categories)):
+            for j in range(0, len(categories) - i - 1):
+                if categories[j].name > categories[j + 1].name:
+                    categories[j], categories[j + 1] = categories[j + 1], categories[j]
 
+
+    def display_all_categories(categories):
+        for category in categories:
+            print(category)
+            print("-" * 50)
+
+    def add_product(products):
+        for product in products:
+            product.category.products.append(product)
 
 
 vehicle = Category("Vehicle")
@@ -49,45 +64,25 @@ suv = Category("SUV", car)
 electric_bike = Category("Electric Bike", bike)
 
 
-vehicle.products = [
-    Product("Pickup Truck ","Price : 48000"), 
-    Product("School Bus ", "Price : 70000"), 
-    Product("Luxury SUV ", "Price : 45000")
-    ]
-
-car.products = [
-    Product("Tesla Model 3 ", "Price : 40000"),
-    Product("Honda Accord ", "Price : 27000"), 
-    Product("Chevrolet Camaro ", "Price : 52000")
-    ]
-
-bike.products = [
-    Product("Royal Enfield ", "Price : 16000"),
-    Product("KTM Duke ", "Price : 18000"), 
-    Product("Suzuki Hayabusa ", "Price : 25000")
-    ]
-
-suv.products = [
-    Product("Range Rover ", "Price : 90000"), 
-    Product("Toyota Fortuner ", "Price : 45000"), 
-    Product("Ford Explorer ", "Price : 60000")
-    ]
-
-electric_bike.products = [
-    Product("Revolt RV400 ", "Price : 12000"), 
-    Product("Ultraviolette F77 ", "Price : 15000"), 
-    Product("Hero Electric Photon ", "Price : 8000")
-    ]
-
+products = [
+    Product("Pickup Truck", vehicle, "Price: 48000"),
+    Product("School Bus", vehicle, "Price: 70000"),
+    Product("Luxury SUV", vehicle, "Price: 45000"),
+    Product("Tesla Model 3", car, "Price: 40000"),
+    Product("Honda Accord", car, "Price: 27000"),
+    Product("Chevrolet Camaro", car, "Price: 52000"),
+    Product("Royal Enfield", bike, "Price: 16000"),
+    Product("KTM Duke", bike, "Price: 18000"),
+    Product("Suzuki Hayabusa", bike, "Price: 25000"),
+    Product("Ford Explorer", suv, "Price: 60000"),
+    Product("Toyota Fortuner", suv, "Price: 45000"),
+    Product("Range Rover", suv, "Price: 90000"),
+    Product("Revolt RV400", electric_bike, "Price: 12000"),
+    Product("Ultraviolette F77", electric_bike, "Price: 15000"),
+    Product("Hero Electric Photon", electric_bike, "Price: 8000")
+]
 
 categories = [vehicle, car, bike, suv, electric_bike]
-
-for i in range(len(categories)):
-    for j in range(0, len(categories) - i - 1):
-        if categories[j].name > categories[j + 1].name:
-            categories[j], categories[j + 1] = categories[j + 1], categories[j]
-
-
-for category in categories:
-    print(category)
-    print("-" * 50)
+Category.sorting_categories(categories)
+Category.add_product(products)
+Category.display_all_categories(categories)
